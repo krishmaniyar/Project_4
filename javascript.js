3497abaeb3165a1198023712fed15b1a9b4a0c99
krish <krishmaniyar27@gmail.com>
2024-09-11 18:17:12 +0530

    index.html

--- javascript.js
+++ javascript.js
@@ -1,6 +1,4 @@
 const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
 
 function makeGrid(rowNum,colNum)
 {
